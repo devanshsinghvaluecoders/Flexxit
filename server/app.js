88ef5bfa0f01e7094a1e8393688082c257cabe81
devanshsinghvaluecoders <devansh.singh@mail.valuecoders.com>
2023-10-31 10:38:18 +0530

    initial changes such as adding styled-component because it is not avail in the package json and add the route in app.js

--- server/app.js
+++ server/app.js
@@ -1,18 +1,23 @@
-import  express  from "express";
+import express from "express";
 import { config } from "dotenv";
 import paymentRoute from "./routes/paymentRoute.js";
 import cors from "cors";
 
-config({path:"./config/config.env"})
+config({ path: "./config/config.env" });
 
-export const app= express();
-app.use(cors({
-    origin:["http://localhost:3000"],
-    methods:["GET","POST"],
-    credentials:true,
-}))
+export const app = express();
+app.use(
+  cors({
+    origin: ["http://localhost:3000"],
+    methods: ["GET", "POST"],
+    credentials: true,
+  })
+);
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
+app.use("/", (req, res) => {
+  res.send("server running");
+});
 app.use("/api", paymentRoute);
 
 app.get("/api/getkey", (req, res) =>
