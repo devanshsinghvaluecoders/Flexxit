d340b21f80e195f091e20645d4bafbcb6b6831a0
devanshsinghvaluecoders <devansh.singh@mail.valuecoders.com>
2023-10-31 11:03:39 +0530

    testing server.js file

--- server/app.js
+++ server/app.js
@@ -15,10 +15,8 @@ app.use(
 );
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use("/", (req, res) => {
-  res.send("server running");
-});
-app.use("/api", paymentRoute);
+
+// app.use("/api", paymentRoute);
 
 app.get("/api/getkey", (req, res) =>
   res.status(200).json({ key: process.env.RAZORPAY_API_KEY })
